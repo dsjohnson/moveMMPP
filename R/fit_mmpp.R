08cabd8e1701bff38bae74eada451e61a2153b6b
Devin Johnson <devin.johnson@noaa.gov>
2022-12-07 16:27:48 -0700

    added functionality to fit_mmpp

--- R/fit_mmpp.R
+++ R/fit_mmpp.R
@@ -8,6 +8,9 @@
 #' @param start Optional starting values for the parameter must be a list of the 
 #' form \code{list(beta_l=c(), beta_q=c())}.
 #' @param method Optimization method. See \code{\link[optimx]{optimr}}
+#' @param fit Logical. Should the liekihood be optimized?
+#' @param debug Integer from 1-5. Opens browser() at various points in the function call. Mostly for 
+#' package developers. 
 #' @param ... Additional arguments passed to the optimization function 
 #' \code{\link[optimx]{optimr}} from the \code{\link[optimx]{optimx-package}}.
 #' @author Devin S. Johnson
@@ -15,10 +18,12 @@
 #' @export
 fit_mmpp <- function(data, ddl, 
                      model_parameters=list(lambda=~1, q=~1), hessian=TRUE,
-                     start=NULL, method="nlminb", ...
+                     start=NULL, method="nlminb", fit=TRUE, debug=0, ...
 ){
   
   cell <- cellx <- fix <- NULL
+  
+  if(debug==1) browser()
   
   cell_idx_df <- select(ddl$lambda, cell, cellx) %>% distinct()
   data <- data %>% left_join(cell_idx_df, by="cell")
@@ -50,7 +55,7 @@ fit_mmpp <- function(data, ddl,
     idx_q = as.integer(dmq_list$idx_q$idx_q-1)
   )
   
-  if(!is.null(start)){
+  if(is.null(start)){
     par_list <- list(
       beta_l=rep(0,ncol(dml_list$X_l)), 
       beta_q=rep(0, ncol(dmq_list$X_q))
@@ -69,25 +74,32 @@ fit_mmpp <- function(data, ddl,
   #   DLL="moveMMPP_TMBExports"
   # )
   
-  message('Optimizing likelihood...')  
-  # opt <- nlminb(start=start, objective=mmpp_ll, data_list=data_list, ...)
-  opt <- optimx::optimr(par=start, fn=mmpp_ll, method=method, data_list=data_list, ...)
+  if(fit){
+    message('Optimizing likelihood...')  
+    if(debug==2) browser()
+    # opt <- nlminb(start=start, objective=mmpp_ll, data_list=data_list, ...)
+    opt <- optimx::optimr(par=start, fn=mmpp_ll, method=method, data_list=data_list, ...)
+    
+    if(opt$convergence!=0){
+      message("There was a problem with optimization... See output 'optimx' object.")
+      return(list(opt=opt, data_list=data_list))
+    }
+    if(hessian){
+      message('Calculating Hessian and variance-covariance matrices...')  
+      H <- numDeriv::hessian(mmpp_ll, opt$par, data_list=data_list)
+      V <- 2*solve(H)
+    }
+  } else{
+    return(list(data_list=data_list, start=par_list))
+  }
   
-  if(opt$convergence!=0){
-    message("There was a problem with optimization... See output 'optimx' object.")
-    return(list(opt=opt, data_list=data_list))
-  }
-  if(hessian){
-    message('Calculating Hessian and variance-covariance matrices...')  
-    H <- numDeriv::hessian(mmpp_ll, opt$par, data_list=data_list)
-    V <- 2*solve(H)
-  }
+  if(debug==4) browser()
   
   ### Get real lambda values
   X_l <- data_list$X_l
   lll <- 1:ncol(X_l)
   beta_l <- opt$par[lll]
-  V_l <- V[lll,lll]
+  if(hessian) V_l <- V[lll,lll]
   l_vals <- exp(X_l %*% beta_l)
   # L <- load_L(data_list$period_l, data_list$cell_l, data_list$idx_l, 
   #                        data_list$fix_l, l_vals, data_list$ns, data_list$np)
@@ -97,48 +109,62 @@ fit_mmpp <- function(data, ddl,
   )
   df_l$real <- l_vals[data_list$idx_l+1]
   df_l$real <- ifelse(is.na(df_l$real), df_l$fix, df_l$real)
-  xbVbx_l <- X_l %*% V_l %*% t(X_l)
-  se_real_l <- as.vector(l_vals) * sqrt(diag(xbVbx_l)) 
-  ci_lower_l <- exp(X_l %*% beta_l - 1.96*sqrt(diag(V_l)))
-  ci_upper_l <- exp(X_l %*% beta_l + 1.96*sqrt(diag(V_l)))
-  df_l$se_real <- se_real_l[data_list$idx_l+1]
-  df_l$se_real <- ifelse(is.na(df_l$se_real) & !is.na(df_l$fix), 0, df_l$se_real)
-  df_l$ci_lower <- ci_lower_l[data_list$idx_l+1]
-  df_l$ci_upper <- ci_upper_l[data_list$idx_l+1]
+  if(hessian){
+    xbVbx_l <- X_l %*% V_l %*% t(X_l)
+    se_real_l <- as.vector(l_vals) * sqrt(diag(xbVbx_l)) 
+    ci_lower_l <- exp(X_l %*% beta_l - 1.96*sqrt(diag(V_l)))
+    ci_upper_l <- exp(X_l %*% beta_l + 1.96*sqrt(diag(V_l)))
+    df_l$se_real <- se_real_l[data_list$idx_l+1]
+    df_l$se_real <- ifelse(is.na(df_l$se_real) & !is.na(df_l$fix), 0, df_l$se_real)
+    df_l$ci_lower <- ci_lower_l[data_list$idx_l+1]
+    df_l$ci_upper <- ci_upper_l[data_list$idx_l+1]
+  }
   df_l <- dplyr::distinct(df_l)
   
   ### Get beta lambda values
   l_nms <- colnames(X_l)
-  df_beta_l <- data.frame(parameter = l_nms, est=beta_l, se_beta=diag(V_l))
+  if(hessian){
+    df_beta_l <- data.frame(parameter = l_nms, est=beta_l, se_beta=diag(V_l))
+  } else{
+    df_beta_l <- data.frame(parameter = l_nms, est=beta_l)
+  }
   
   ### Get real Q  values
   X_q <- data_list$X_q
   qqq <- (ncol(X_l)+1):(ncol(X_l)+ncol(X_q))
   beta_q <- opt$par[qqq]
-  V_q <- V[qqq,qqq]
+  if(hessian) V_q <- V[qqq,qqq]
   from_to_q <- t(cbind(data_list$from, data_list$to))
   q_vals <- exp(X_q %*% beta_q)
   q_nms <- colnames(X_q)
   # Q <- load_Q(from_to_q, data_list$idx_q, q_vals, data_list$ns)
   df_q <- model.frame(model_parameters$q, ddl$q)
   df_q$real <- q_vals[data_list$idx_q+1]
-  xbVbx_q <- X_q %*% V_q %*% t(X_q)
-  se_real_q <- as.vector(q_vals) * sqrt(diag(xbVbx_q)) 
-  df_q$se_real <- se_real_q[data_list$idx_q+1]
-  ci_lower_q <- exp(X_q %*% beta_q - 1.96*sqrt(diag(V_q)))
-  ci_upper_q <- exp(X_q %*% beta_q + 1.96*sqrt(diag(V_q)))
-  df_q$ci_lower <- ci_lower_q[data_list$idx_q+1]
-  df_q$ci_upper <- ci_upper_q[data_list$idx_q+1]
+  if(hessian){
+    xbVbx_q <- X_q %*% V_q %*% t(X_q)
+    se_real_q <- as.vector(q_vals) * sqrt(diag(xbVbx_q)) 
+    df_q$se_real <- se_real_q[data_list$idx_q+1]
+    ci_lower_q <- exp(X_q %*% beta_q - 1.96*sqrt(diag(V_q)))
+    ci_upper_q <- exp(X_q %*% beta_q + 1.96*sqrt(diag(V_q)))
+    df_q$ci_lower <- ci_lower_q[data_list$idx_q+1]
+    df_q$ci_upper <- ci_upper_q[data_list$idx_q+1]
+  }
   df_q <- dplyr::distinct(df_q)
   
   ### Get beta Q values
   q_nms <- colnames(X_q)
-  df_beta_q <- data.frame(parameter = q_nms, est=beta_q, se_beta=diag(V_q))
+  if(hessian){
+    df_beta_q <- data.frame(parameter = q_nms, est=beta_q, se_beta=diag(V_q))
+  } else{
+    df_beta_q <- data.frame(parameter = q_nms, est=beta_q)
+  }
   
   ####
   # statd <- eigen(t(Q))$vectors[,78] /sum(eigen(t(Q))$vectors[,78])
   # zones$ppp <- statd
   # mapview::mapview(zones, zcol='ppp')
+  
+  if(!hessian) V <- NULL
   
   out <- list(
     par = c(beta_l,beta_q),
@@ -158,6 +184,8 @@ fit_mmpp <- function(data, ddl,
     start=start,
     data_list=data_list
   )
+  
+  if(debug==5) browser()
   
   return(out)
   
